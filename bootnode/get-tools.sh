0e82738947a1d43fe3ed124d3e2c2cf396441109
Daniel Perez <daniel@perez.sh>
2018-10-22 20:01:55 +0100

    Use previous version of bootnode

--- bootnode/get-tools.sh
+++ bootnode/get-tools.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-TARDIR=geth-alltools-linux-amd64-1.8.18-unstable-cdf5982c
+TARDIR=geth-alltools-linux-amd64-1.8.17-8bbe7207
+# TARDIR=geth-alltools-linux-amd64-1.8.18-unstable-cdf5982c
 TARFILE=$TARDIR.tar.gz
 
 if ! [ -f "$TARFILE" ]; then
-  wget https://gethstore.blob.core.windows.net/builds/geth-alltools-linux-amd64-1.8.18-unstable-cdf5982c.tar.gz
+  wget https://gethstore.blob.core.windows.net/builds/$TARFILE
 fi
 
 if ! [ -d "$TARDIR" ]; then
-  tar xvzf geth-alltools-linux-amd64-1.8.18-unstable-cdf5982c.tar.gz
+  tar xvzf $TARFILE
 fi
 
 if ! [ -f "bootnode" ]; then
